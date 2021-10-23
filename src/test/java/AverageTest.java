46befb48eb491548a1ba36d346cd572765dbf045
Matthew Nguyen <89543301+matthewpop1@users.noreply.github.com>
2021-10-22 21:44:52 -0500

    Initial commit

---
+++ src/test/java/AverageTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AverageTest {
+    @Test
+    void averageTest1() {
+        Assertions.assertEquals(11, Average.average(-2,new int[]{2,4,6,8,10,12,14,16,18,20}));
+    }
+    @Test
+    void averageTest2() {
+        Assertions.assertEquals(11, Average.average(-2,new int[]{2,4,6,8,10,12,14,16,18,20}));
+    }
+    @Test
+    void averageTest3() {
+        Assertions.assertEquals(11, Average.average(-2,new int[]{2,4,6,8,10,12,14,16,18,20}));
+    }
+    @Test
+    void averageTest4() {
+        Assertions.assertEquals(11, Average.average(-2,new int[]{2,4,6,8,10,12,14,16,18,20}));
+    }
+    @Test
+    void averageTest5() {
+        Assertions.assertEquals(11, Average.average(-2,new int[]{2,4,6,8,10,12,14,16,18,20}));
+    }
+}
